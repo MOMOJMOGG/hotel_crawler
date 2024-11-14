fd04ee63d153ba2ddee90e6e58c30427a9d8fc85
MOMOJMOGG <momomacaca@gmail.com>
2024-11-14 21:11:26 +0800

    [feat](dashboard): add streamlit dashboard design
    * add review_analyzer for data parsing
    * build streamlit dashboard interactive page

--- main.py
+++ main.py
@@ -1,51 +1,233 @@
+from functools import reduce
 import streamlit as st
-import numpy as np
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.rc('font', family='Microsoft YaHei') #? 設定 中文顯示字體
 
 import os
+import time
+
 from modules.settings.config_manager import config
 config.load_settings(os.path.dirname(os.path.abspath(__file__)))
+from modules.review_analizer import analyzer
     
 from modules.gmap_crawler import craw_hotel
 from modules.data_handler import DataHandler
 
 if __name__ == '__main__':
-    
     st.title('評論分析網')
-
-    hotel_name = st.text_input('請輸入欲查詢之飯店名稱')
     
-    if st.button('生成評論分析報表'):
-        data = craw_hotel(hotel_name)
-        
+    st.text_input('請輸入欲查詢之飯店名稱', key='hotel_name')
+    
+    if st.button('生成評論分析報表') or st.session_state.hotel_name:
         st.divider()
-        st.header("飯店資訊", divider='red')
-        st.markdown(f"**Hotel**: {data['name']}")
-        st.markdown(f"**Star** : {data['star']}")
-        st.markdown(f"**Rank** : {data['rank']}")
-        st.markdown(f"**Address**: {data['information'].get('address', '---')}")
-        st.markdown(f"**URL**  : {data['information'].get('url', '---')}")
-        st.markdown(f"**Phone**: {data['information'].get('phone', '---')}")
-        if data['information'].get('time'):
-            st.markdown(f"**Check In**: {data['information']['time'].get('start', '---')}")
-            st.markdown(f"**Check Out**: {data['information']['time'].get('end', '---')}")
+        
+        with st.status("執行評論爬蟲中，請稍後...", expanded=True) as crawler_status:
+            start = time.time()
+            
+            #* keep data to prevent rerun affect
+            if 'data_name' not in st.session_state or st.session_state.data_name != st.session_state.hotel_name:
+                data = craw_hotel(st.session_state.hotel_name)
+                
+                #* set hotel name -> data_name | crawler data -> hotel_data
+                st.session_state.data_name = st.session_state.hotel_name
+                st.session_state.hotel_data = data
+            
+            else:
+                #* reuse storaged data
+                data = st.session_state.hotel_data
+            
+            # === Information ===
+            st.header("飯店資訊", divider='red')
+            st.markdown(f"**Hotel**: {data['name']}")
+            st.markdown(f"**Star** : {data['star']}")
+            st.markdown(f"**Rank** : {data['rank']}")
+            st.markdown(f"**Address**: {data['information'].get('address', '---')}")
+            st.markdown(f"**URL**  : {data['information'].get('url', '---')}")
+            st.markdown(f"**Phone**: {data['information'].get('phone', '---')}")
+            if data['information'].get('time'):
+                st.markdown(f"**Check In**: {data['information']['time'].get('start', '---')}")
+                st.markdown(f"**Check Out**: {data['information']['time'].get('end', '---')}")
 
-        st.subheader("原始資料", divider=True)
+            # === Table ===
+            st.subheader("原始資料", divider=True)
+            handler = DataHandler(data['review'])
+            st.dataframe(handler.df, use_container_width=True)
         
-        handler = DataHandler(data['review'])
-        st.dataframe(handler.df, use_container_width=True)
+            handler.exec_clean('comment')
+            st.subheader("清理資料", divider=True)
+            st.dataframe(handler.df, use_container_width=True)
+
+            # === Time Cost ===
+            if st.session_state.data_name == st.session_state.hotel_name:
+                st.markdown(f"**爬蟲耗時**: :red[{time.time() - start}] s, 共 :green[{len(data['review'])}] 筆評論")
+            
+            crawler_status.update(
+                label="評論爬蟲完成", state="complete", expanded=True
+            )
         
-        handler.exec_clean('comment')
-        st.subheader("清理資料", divider=True)
-        st.dataframe(handler.df, use_container_width=True)
+        with st.status("執行評論分析，分析耗時較久，請耐心等候...") as analyze_status:
+            start = time.time()
+            
+            #* keep data to prevent rerun affect
+            if 'analyze_name' not in st.session_state or st.session_state.analyze_name != st.session_state.hotel_name:
+                #> call open ai api to analyze reviews
+                handler.exec_analyze('comment')
+                end_analyze = time.time()
+                
+                #* set hotel name -> analyze_name | formed data -> analyze_data
+                st.session_state.analyze_name = st.session_state.hotel_name
+                st.session_state.analyze_data = handler.formed_data
+                
+                formed_data = handler.formed_data
+            
+            else:
+                #* reuse storaged data
+                formed_data = st.session_state.analyze_data
+
+            # === Parse formed data to tables ===
+            st.subheader("分析資料", divider=True)
+            formed_df = pd.DataFrame(formed_data)
+            st.dataframe(formed_df, use_container_width=True)
+
+            #> 關鍵字統計
+            calc_result = analyzer.parse_key_words(formed_data)
         
-        st.divider()
-        st.markdown('''
-                    :red[分析資料會耗時較久, 請耐心等候]
-                    ''')
-        handler.exec_analyze('comment')
-        st.subheader("分析資料", divider=True)
-        formed_df = pd.DataFrame(handler.formed_data)
-        st.dataframe(formed_df, use_container_width=True)
+            positive_counter = calc_result.get('positive')
+            negative_counter = calc_result.get('negative')
+            common_set = calc_result.get('common')
+            summary_counter = calc_result.get('summary')
         
+            # 只保留關鍵字被提及超過一次的
+            positive_counter = analyzer.filter_freq(positive_counter, 1)
+            negative_counter = analyzer.filter_freq(negative_counter, 1)
+            
+            # 取得 關鍵字 名字與顏色 對照表
+            summary_options  = analyzer.build_multi_select_option(summary_counter.keys())
+            positive_options = analyzer.build_multi_select_option(positive_counter.keys())
+            negative_options = analyzer.build_multi_select_option(negative_counter.keys())
+
+            # 顯示關鍵字保留選項
+            positive_content = "**正面關鍵字**"
+            for option in positive_options:
+                positive_content += f" :{option['color']}[{option['name']}]"
+            st.markdown(positive_content)
+            
+            negative_content = "**負面關鍵字**"
+            for option in negative_options:
+                negative_content += f" :{option['color']}[{option['name']}]"
+            st.markdown(negative_content)
+            
+            # 建置過濾按鈕
+            st.pills("推薦度",
+                     summary_counter.keys(),
+                     selection_mode='single',
+                     key='sum_option')
+            
+            st.pills("正面關鍵字",
+                     positive_counter.keys(),
+                     selection_mode="multi",
+                     key='pos_options')
+            
+            st.pills("負面關鍵字",
+                     negative_counter.keys(),
+                     selection_mode="multi",
+                     key='neg_options')
+            
+            # 解析數據成表格
+            filterd_key = {
+                'positive': positive_counter.keys(),
+                'negative': negative_counter.keys()
+            }
+            spec_data = analyzer.parse_data_to_db_spec(formed_data, filterd_key)
+            data_list = analyzer.build_table(spec_data)
         
+            st.subheader("整理資料", divider=True)
+            
+            #* 執行過濾
+            final_df = pd.DataFrame(data_list)
+            if '推薦度' in final_df.columns and st.session_state.sum_option:
+                sum_option = '不推' if st.session_state.sum_option == '不推薦' else st.session_state.sum_option
+                sum_condition = [final_df['推薦度'].str.contains(sum_option, na=False)]
+                final_df = final_df[reduce(lambda x, y: x & y, sum_condition)]
+            
+            if '正面關鍵字' in final_df.columns and st.session_state.pos_options:
+                pos_condition = [final_df['正面關鍵字'].str.contains(option, na=False) for option in st.session_state.pos_options]
+                final_df = final_df[reduce(lambda x, y: x & y, pos_condition)]
+
+            if '負面關鍵字' in final_df.columns and st.session_state.neg_options:
+                neg_condition = [final_df['負面關鍵字'].str.contains(option, na=False) for option in st.session_state.neg_options]
+                final_df = final_df[reduce(lambda x, y: x & y, neg_condition)]
+    
+            # 顯示過濾表格
+            st.dataframe(final_df, use_container_width=True)
+
+            # === 圓餅圖 ===
+            st.subheader("總結", divider=True)
+            summary_data = {
+                'label': summary_counter.keys(),
+                'value': summary_counter.values()
+            }
+            
+            positive_data = {
+                'label': positive_counter.keys(),
+                'value': positive_counter.values()
+            }
+            
+            negative_data = {
+                'label': negative_counter.keys(),
+                'value': negative_counter.values()
+            }
+            
+            st.text("總評價比例")
+            
+            #> 關鍵字計數
+            sum_content = ""
+            for option in summary_options:
+                sum_content += "{}: :{}[{}]\n".format(option['name'],
+                                                      option['color'],
+                                                      summary_counter[option['name']])
+            st.markdown(sum_content)
+            
+            plt.pie(summary_data['value'], labels=summary_data['label'], autopct='%1.1f%%')
+            st.pyplot(plt)
+            plt.clf()
+            
+            st.text("正向關鍵字比例")
+            
+            #> 關鍵字計數
+            pos_content = ""
+            for option in positive_options:
+                pos_content += "{}: :{}[{}]\n".format(option['name'],
+                                                      option['color'],
+                                                      positive_counter[option['name']])
+            st.markdown(pos_content)
+            
+            plt.pie(positive_data['value'], labels=positive_data['label'], autopct='%1.1f%%')
+            st.pyplot(plt)
+            plt.clf()
+            
+            st.text("負向關鍵字比例")
+            
+            #> 關鍵字計數
+            neg_content = ""
+            for option in negative_options:
+                neg_content += "{}: :{}[{}]\n".format(option['name'],
+                                                      option['color'],
+                                                      negative_counter[option['name']])
+            st.markdown(neg_content)
+            
+            plt.pie(negative_data['value'], labels=negative_data['label'], autopct='%1.1f%%')
+            st.pyplot(plt)
+            plt.clf()
+            
+            end_parse = time.time()
+            
+            if st.session_state.analyze_name != st.session_state.hotel_name:
+                st.markdown(f"**分析耗時**: :red[{end_analyze - start}] s, **繪製數據圖表耗時**: :red[{end_parse - end_analyze}] s")
+            
+            analyze_status.update(
+                label="評論分析完成", state="complete", expanded=True
+            )
+            
--- modules/review_analizer.py
+++ modules/review_analizer.py
@@ -1,6 +1,8 @@
-from modules.settings.config_manager import config
+from collections import Counter
+import json
 from openai import OpenAI
 from pydantic import BaseModel
+from modules.settings.config_manager import config
 
 
 class ReviewAnalyzer:
@@ -24,5 +26,135 @@ class ReviewAnalyzer:
     
         else:
             return {}
+    
+    # 取得 統計資料: 正面關鍵字 / 負面關鍵字 / 共同關鍵字 / 總評價
+    def parse_key_words(self, data):
+        positive_keys = []
+        negative_keys = []
+        summary_keys  = []
+        
+        for data_row in data:
+            json_data = json.loads(data_row)
+            
+            analyze_data = json_data.get('評論分析')
+            positive_list = analyze_data.get('正向評論')
+            positive_keys.extend([item.get('關鍵字') for item in positive_list])
+            
+            negative_list = analyze_data.get('負向評論')
+            negative_keys.extend([item.get('關鍵字') for item in negative_list])
+            
+            summary_data = json_data.get('評論總結')
+            summary_keys.append(summary_data.get('評價'))
 
+        positive_counts = Counter(positive_keys)
+        negative_counts = Counter(negative_keys)
+        summary_counts  = Counter(summary_keys)
+            
+        # 找出重複的正負向關鍵字
+        common_keywords = set(positive_counts.keys()).intersection(set(negative_counts.keys()))
+
+        return {
+            'positive': positive_counts,
+            'negative': negative_counts,
+            'common': common_keywords,
+            'summary': summary_counts
+        }
+    
+    def filter_freq(self, data, minimum):
+        result = {}
+        for key, cnt in data.items():
+            if cnt > minimum:
+                result[key] = cnt
+        
+        return result
+
+    def build_multi_select_option(self, options):
+        color = ["red",  "orange", "green",
+                "blue", "violet", "gray"]      
+          
+        result = []
+        for idx, option in enumerate(options):
+            result.append({
+                'name': option,
+                'color': color[idx % 6]
+            })
+        
+        return result
+    
+    # 解析資料成頁面的需求規格
+    def parse_data_to_db_spec(self, data, filterd_key):
+        positive_keys = filterd_key.get('positive')
+        negative_keys = filterd_key.get('negative')
+        
+        spec_data = []
+        
+        for idx, data_row in enumerate(data):
+            json_data = json.loads(data_row)
+
+            info = {}
+            info['ID'] = f'{idx + 1}'
+            info['review'] = json_data.get('評論')
+            
+            analyze_data = json_data.get('評論分析')
+            positive_list = analyze_data.get('正向評論')
+            negative_list = analyze_data.get('負向評論')
+            
+            info['positive'] = []
+            for item in positive_list:
+                if item.get('關鍵字') in positive_keys:
+                    info['positive'].append(item.get('關鍵字'))
+                    
+            info['negative'] = []
+            for item in negative_list:
+                if item.get('關鍵字') in negative_keys:
+                    info['negative'].append(item.get('關鍵字'))
+                    
+            summary_data = json_data.get('評論總結')
+            info['star'] = summary_data.get('評價')
+            info['recommand'] = summary_data.get('推薦結論')
+
+            spec_data.append(info)
+            
+        return spec_data
+
+    def build_table(self, data_list):
+        star_map = {
+            '推薦': '推薦 ⭐⭐⭐',
+            '普通': '普通 ⭐⭐',
+            '不推薦': '不推 ⭐'
+        }
+        
+        result = []
+        
+        for data in data_list:
+            positive_list = data.get('positive')
+            negative_list = data.get('negative')
+            positive_num = len(positive_list)
+            negative_num = len(negative_list)
+            
+            # 評價總數
+            total_num = positive_num + negative_num
+            if total_num == 0:
+                continue
+            
+            # 評價比例
+            ratio = round((positive_num / total_num) * 100, 2)
+            
+            # 總評價
+            star = star_map[data.get('star')]
+
+            result.append({
+                'ID': data.get('ID'),
+                '推薦度': star,
+                '推薦結論': data.get('recommand'),
+                '正面評價數': positive_num,
+                '評價比例': ratio,
+                '負面評價數': negative_num,
+                '正面關鍵字': ",".join(positive_list),
+                '負面關鍵字': ",".join(negative_list),
+                '評論': data.get('review')
+            })
+            
+        return result
+    
 analyzer = ReviewAnalyzer()
